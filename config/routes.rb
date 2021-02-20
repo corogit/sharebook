2cef3c016112970f74fbc88cacc5cdeffb257bb1
corogit <n.ohta5192@gmail.com>
2021-02-20 12:36:17 +0000

    [add]devise:admins

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,22 @@
 Rails.application.routes.draw do
-  devise_for :users
-  root to: to
+  devise_for :admins, controllers: {
+    sessions:      'admins/sessions',
+    passwords:     'admins/passwords',
+    registrations: 'admins/registrations'
+  }
+  devise_for :users, controllers: {
+    sessions:      'users/sessions',
+    passwords:     'users/passwords',
+    registrations: 'users/registrations'
+  }
+  
+  namespace :admin do
+    get '/' => 'homes#top'
+  end
+  
+  
+  scope module: :user do
+    get '/' => 'homes#top'
+    resources :homes, only: [:top, :about]
+  end
 end

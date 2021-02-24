fc21f43e186aaf1ce61da0d0ba75c45f6ff150db
corogit <n.ohta5192@gmail.com>
2021-02-24 11:03:41 +0000

    [install]refile

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,12 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+         
+ has_many :books, dependent: :destroy
+ has_many :likes, dependent: :destroy
+
+ #既にいいねしているかどうか
+ def already_liked?(item)
+   self.likes.exists?(item_id: item.id)
+ end
 end

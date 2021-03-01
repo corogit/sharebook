8aa6c8d6551a25569585dec1746c96ab712688d6
corogit <n.ohta5192@gmail.com>
2021-03-01 14:11:29 +0000

    [add]comment機能

---
+++ app/models/comment.rb
@@ -0,0 +1,6 @@
+class Comment < ApplicationRecord
+  belongs_to :user
+  belongs_to :book
+  #バリデーション
+  validates :content, presence: true
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_02_25_131346) do
+ActiveRecord::Schema.define(version: 2021_03_01_140930) do
 
   create_table "admins", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -29,6 +29,14 @@ ActiveRecord::Schema.define(version: 2021_02_25_131346) do
     t.text "body"
     t.string "author"
     t.integer "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "comments", force: :cascade do |t|
+    t.text "content"
+    t.integer "user_id"
+    t.integer "book_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
